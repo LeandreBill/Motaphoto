b3f713457c8d9361f4a81e0198499448d6912f70
Leandre Deck <leandredeck@gmail.com>
2023-11-28 16:34:12 +0100

    Création du thème

---
+++ Motaphoto_theme/functions.php
@@ -0,0 +1,36 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+?>
+
+<?php
+
+// Ajouter la prise en charge des images mises en avant
+add_theme_support( 'post-thumbnails' );
+
+// Ajouter automatiquement le titre du site dans l'en-tête du site
+add_theme_support( 'title-tag' );
+
+//ajouter le logo
+add_theme_support('custom-logo');
+
+// Enqueue le fichier style.css du thème
+function enqueue_motaphoto_styles() {
+    wp_enqueue_style('motaphoto-style', get_stylesheet_uri());
+}
+
+// Action pour ajouter la fonction à la file d'attente des styles
+add_action('wp_enqueue_scripts', 'enqueue_motaphoto_styles');
+
+// enregistrement des emplacements de menus
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'main-menu' => __('Menu principal', 'text-domain'),
+            'footer-menu' => __('Menu pied de page', 'text-domain'),
+        )
+    );
+}
+add_action('after_setup_theme', 'register_my_menus');
