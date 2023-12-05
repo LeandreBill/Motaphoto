ce4a06225791041274dd3bf38adf5f9f8bdcc395
Leandre Deck <leandredeck@gmail.com>
2023-12-05 13:48:45 +0100

    Appel Ajax et lightbox
    bouton charger plus, contact

---
+++ Motaphoto_theme/assets/js/lightbox.js
@@ -0,0 +1,45 @@
+// 12.lightbox
+
+jQuery(document).ready(function ($) {
+    var $lightbox = $("#lightbox");
+    var currentIndex = -1;
+  
+    function displayImage(index) {
+      var $photos = $(".photo-items");
+      if (index < 0 || index >= $photos.length) return;
+  
+      var $photoItem = $photos.eq(index);
+      var imageSrc = $photoItem.find(".fullscreen-icon").data("image");
+      var reference = $photoItem.find(".fullscreen-icon").data("reference");
+      var category = $photoItem.find(".photo-icons").data("cat");
+  
+      $(".lightbox-content img").attr("src", imageSrc);
+      $(".photo-ref").text(reference);
+      $("#lightbox-category").text(category);
+  
+      currentIndex = index;
+    }
+  
+    $("body").on("click", ".fullscreen-icon", function (e) {
+      e.preventDefault();
+  
+      var $photos = $(".photo-items");
+      var index = $photos.index($(this).closest(".photo-items"));
+      displayImage(index);
+  
+      $lightbox.show();
+    });
+  
+    $("#close-lightbox").on("click", function () {
+      $lightbox.hide();
+      $(".lightbox-content img").attr("src", "");
+    });
+  
+    $("#prev-image").on("click", function () {
+      displayImage(currentIndex - 1);
+    });
+  
+    $("#next-image").on("click", function () {
+      displayImage(currentIndex + 1);
+    });
+  });
---
+++ Motaphoto_theme/assets/js/load-more.js
@@ -0,0 +1,47 @@
+// 11. appel du bouton en Ajax 
+
+jQuery(document).ready(function ($) {
+    let currentPage = 1;
+  
+    $("#load-more").on("click", function () {
+      currentPage++; // Incrémentation de currentPage de 1, pour charger la page suivante
+  
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url,
+        dataType: "json",
+        data: {
+          action: "load_more_photos",
+          paged: currentPage,
+        },
+        success: function (res) {
+          $(".photo-gallery").append(res.html); // Ajoute le contenu à la galerie existante
+  
+          if (currentPage >= res.max) {
+            $("#load-more").hide();
+          }
+        },
+      });
+    });
+  });
+  
+  // bouton charger plus de la single page
+  jQuery(document).ready(function ($) {
+    $("#load-more-single").on("click", function () {
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url,
+        dataType: "json",
+        data: {
+          action: "load_all_photos",
+        },
+        success: function (res) {
+          $(".photo-suggest").html(res.html); // Remplace le contenu actuel par les nouvelles photos
+  
+          if (res.allLoaded) {
+            $("#load-more-single").hide(); // Masque le bouton si toutes les photos sont chargées
+          }
+        },
+      });
+    });
+  });
